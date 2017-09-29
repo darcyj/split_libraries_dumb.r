24c57becd01bd7c62a1e1b8d863b6a0a2836db1e
John L Darcy <jack.l.darcy@gmail.com>
2017-09-29 15:59:39 -0600

    Add files via upload
    added ability to reverse-compliment sequences found in mapping file
    
    added option to put casava 1.8 metadata at ends of seq ids
    
    fixed bug where name of R1 read was capitalized and R2 read wasn't

--- split_libraries_dumb.r
+++ split_libraries_dumb.r
@@ -1,13 +1,8 @@
 #!/usr/bin/Rscript
 
 # Jack Darcy
-# 24 AUG 2017
-# Script to demultiplex illumina raw fastq files WITHOUT filtering
-# I wrote this program because I want to demultiplex, THEN join my paired-end files. 
-# That's impossible just using qiime - you have to join first, since split_libraries_fastq.py
-# filters R1 and R2 indipendently. Then, you get two fastqs of different lengths, and now
-# you can't use vsearch anymore if you want to join them together. You COULD use the fastx 
-# toolkit to do this, but it's SLOW and hard to understand. This is FAST. WAY FAST. -Jack 
+# 19 JAN 2014
+# script to filter QIIME format OTU maps
 
 # usage: split_libraries_dumb.r --r1 r1.fastq --r2 r2.fastq -i index.fastq -m mappintgile.txt
 
@@ -22,10 +17,12 @@ option_list <- list(
 			help="Index reads in fastq format"),
 	make_option(c("-m", "--map"), action="store", default=NA, type='character',
 			help="QIIME format mapping file"),
-	make_option(c("-o", "--output_directory"), action="store", default="", type='character',
-			help="Directory where you want output fastq files to be. Default=pwd."),
 	make_option("--skip", action="store", default="none", type='character',
 			help="Used to skip 'first' or 'last' character of index read."),
+	make_option("--rc_barcodes", action="store_true", default=FALSE, type='logical',
+		help="Reverse-compliments your barcodes before anything else."),
+	make_option("--add_Cas1.8_data", action="store_true", default=FALSE, type='logical',
+		help="Adds Casava 1.8 tags to sequence names (1:N:0:ATGATATGATGA)"),
 	make_option("--prefix", action="store", default="filtered", type='character',
 		help="Prefix for output fastq files. Will be appended to _r[12].fastq")
 # help option -h/--help is included by optparse by default
@@ -39,16 +36,32 @@ r2_fp <- opt$r2
 in_fp <- opt$index
 mf_fp <- opt$map
 outprefix <- opt$prefix
-outdir <- opt$output_directory
+rc_barcodes <- opt$rc_barcodes
+cas18names <- opt$add_Cas1.8_data
 
-# strip terminal "/" characters from outdir
-# don't actually need to do this, but it's nice when things look right
-while(substr(outdir, start=nchar(outdir), stop=nchar(outdir)) == "/"){
-	outdir <- substr(outdir, start=1, stop=nchar(outdir) - 1)
+# functions to reverse compliment a string
+# functions to reverse-compliment a sequence
+complimnet <- function(x){
+	if(x == "C"){out <- "G"}
+	if(x == "G"){out <- "C"}
+	if(x == "A"){out <- "T"}
+	if(x == "T"){out <- "A"}
+	if(!x %in% c("A", "T", "G", "C")){out <- x}
+	return(out)
+}
+rc <- function(x){
+	if(length(x) > 0){
+		for(i in 1:length(x)){
+			tmp_i <- rev(unlist(strsplit(x[i], split="")))
+			x[i] <- paste(sapply(tmp_i, FUN=complimnet), collapse="")
+		}
+	}
+	return(x)
 }
 
+
 # test that all input files have the right number of lines BEFORE
-# reading them into memory. This is only tested on linux. 
+# reading them into memory
 print("Verifyinng file lengths.")
 linecount <- function(fp){
 	command <- paste("wc -l", fp, "| cut -f 1 -d ' '")
@@ -83,6 +96,13 @@ if(skip == "last"){
 print("Reading mapping file.")
 map <- fread(mf_fp, header=T, stringsAsFactors=FALSE, sep='\t')
 
+if(rc_barcodes){
+	print("Reverse-complementing barcodes within mapping file.")
+	for(i in 1:nrow(map)){
+		map$BarcodeSequence[i] <- rc(map$BarcodeSequence[i])	
+	}
+}
+
 # compare index reads to barcodes
 print("Comparing index reads to barcodes.")
 good_seqs <- index[[1]] %in% map$BarcodeSequence
@@ -98,23 +118,26 @@ bc2sampid <- function(good_index, trans_df){
 	}
 	return(samp)
 }
-sampids <- character(length(good_seqs))
+
+# for each read, figure out its sampleID and its index sequence
+sampids <- indices <- character(length(good_seqs))
 for(i in 1:nrow(map)){
 	bc_i <- map$BarcodeSequence[i]
 	samp_i <- map[[1]] [i]
 	sampids[which(index[[1]] == bc_i)] <- samp_i
+	indices[which(index[[1]] == bc_i)] <- bc_i
 }
-
 rm(index)
 sampids_good <- sampids[good_seqs]
+indices_good <- indices[good_seqs]
 good_lines <- rep(good_seqs, each=4)
 
 # status message
 print(paste("Found", sum(good_seqs), "hits out of", length(good_seqs), "total."))
 
 # get ready for filtering
-r1_outname <- paste(outdir, "/", outprefix, "_r1.fastq", sep="")
-r2_outname <- paste(outdir, "/", outprefix, "_r2.fastq", sep="")
+r1_outname <- paste(outprefix, "_r1.fastq", sep="")
+r2_outname <- paste(outprefix, "_r2.fastq", sep="")
 
 # filter r1
 print("Reading in R1 fastq file.")
@@ -122,6 +145,7 @@ r1_fastq <- fread(r1_fp, header=F, stringsAsFactors=FALSE, sep='\t')
 print("Filtering R1 fastq file.")
 r1_fastq <- r1_fastq[[1]][good_lines]
 r1_newnames <- paste("@", sampids_good, "_R1-", (1:length(sampids_good)), sep="")
+if(cas18names){r1_newnames <- paste(r1_newnames, " 1:N:2:", indices_good, sep="")}
 r1_fastq[(1:length(r1_fastq) + 3) %% 4 == 0] <- r1_newnames
 print("Writing out R1 fastq file.")
 fwrite(list(r1_fastq), file=r1_outname, quote=F)
@@ -132,12 +156,11 @@ print("Reading in R2 fastq file.")
 r2_fastq <- fread(r2_fp, header=F, stringsAsFactors=FALSE, sep='\t')
 print("Filtering r2 fastq file.")
 r2_fastq <- r2_fastq[[1]][good_lines]
-r2_newnames <- paste("@", sampids_good, "_r2-", (1:length(sampids_good)), sep="")
+r2_newnames <- paste("@", sampids_good, "_R2-", (1:length(sampids_good)), sep="")
+if(cas18names){r2_newnames <- paste(r2_newnames, " 2:N:2:", indices_good, sep="")}
 r2_fastq[(1:length(r2_fastq) + 3) %% 4 == 0] <- r2_newnames
 print("Writing out r2 fastq file.")
 fwrite(list(r2_fastq), file=r2_outname, quote=F)
-rm(r2.fastq)
+rm(r2_fastq)
 
-print("All done. Files written:")
-print(r1_outname)
-print(r2_outname)
+
